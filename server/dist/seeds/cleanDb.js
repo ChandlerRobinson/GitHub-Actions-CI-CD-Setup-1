00d8ae847e34b236757f97afc8d0941a2db29961
ChandlerRobinson <chandlerbronsonrobinson@gmail.com>
2024-12-04 08:49:14 -0500

    npm installed in root, files moved to root

---
+++ server/dist/seeds/cleanDb.js
@@ -0,0 +1,20 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+// import { model, modelNames } from 'mongoose';
+export default async (modelName, collectionName) => {
+    try {
+        let modelExists = models[modelName];
+        if (!modelExists || !modelExists.db?.db) {
+            throw new Error("model not found");
+        }
+        let exists = await modelExists.db.db.listCollections({
+            name: collectionName
+        }).toArray();
+        if (exists.length) {
+            await db.dropCollection(collectionName);
+        }
+    }
+    catch (err) {
+        throw err;
+    }
+};
